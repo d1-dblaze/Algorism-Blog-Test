b6b4f42266b447321b45d0626025bf48f0cf9c98
d1-dblaze <adedayoajao7@gmail.com>
2021-08-04 12:52:23 +0100

    added restframework and corsheaders

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -38,9 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myblog.apps.MyblogConfig',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,3 +123,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+           'rest_framework.permissions.AllowAny',
+    ]
+}
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:3000/'       #for the react app
+)
---
+++ myblog/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Post,Comment
+
+class PostSerializer (serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            'id',
+            'title',
+            'description',
+            'slug',
+            'author',
+            'body',
+            'publish'
+        )
